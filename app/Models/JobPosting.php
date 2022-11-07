daa2df4de64ac88d9ac2e723ff15018ad255c42b
FKingQueen <luxborj@gmail.com>
2022-11-07 16:12:29 +0800

    Initial Commit

---
+++ app/Models/JobPosting.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JobPosting extends Model
+{
+    use HasFactory;
+        /** 
+     * Display a listing of the resource. 
+     * @return \Illuminate\Http\Response 
+     */  
+public function index()  
+{  
+          
+//  
+      
+}  
+/** 
+     * Show the form for creating a new resource. 
+     * @return \Illuminate\Http\Response 
+       
+*/  
+ public function create()  
+{  
+          
+//  
+      
+}  
+/** 
+     * Store a newly created resource in storage. 
+     * 
+     * @param  \Illuminate\Http\Request   $request 
+     * @return \Illuminate\Http\Response 
+     */  
+public function store(Request $request)  
+{  
+          
+//  
+      
+}  
+/** 
+     * Display the specified resource. 
+     * @param  int  $id 
+     * @return \Illuminate\Http\Response 
+     */  
+public function show($id)  
+{  
+  
+  //  
+}  
+/** 
+     * Show the form for editing the specified resource. 
+     * @param  int  $id 
+     * @return  \Illuminate\Http\Response 
+     */  
+ public function edit($id)  
+      
+{  
+          
+//  
+      
+}  
+/** 
+     * Update the specified resource in storage. 
+     * @param  \Illuminate\Http\Request   $request 
+     * @param  int  $id 
+     * @return \Illuminate\Http\Response 
+     */  
+ public function update(Request $request, $id)  
+{  
+          
+//  
+      
+}  
+/** 
+     * Remove the specified resource from storage. 
+     * @param  int  $id 
+     * @return  \Illuminate\Http\Response 
+     */  
+ public function destroy($id)  
+ {  
+          
+}
+}
